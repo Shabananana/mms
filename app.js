ed7cb3b056b4897677f9b978abfbd37636b0c7fe
Shabananana <jeff.davis@karmaloop.com>
2013-02-17 10:42:49 -0500

    Added passport and basic markup to allow for login

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var express = require('express')
   , user = require('./routes/user')
   , image = require('./routes/image')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
@@ -33,10 +35,30 @@ app.configure('development', function(){
 
 app.use(express.bodyParser({uploadDir:'./uploads'}));
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/image', image.createImageForm);
 app.post('/image/post', image.createImagePost);
+app.post('/login',
+  passport.authenticate('local',
+    { successRedirect: '/',
+      failureRedirect: '/login',
+      failureFlash: true }));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
